55ca4cb0b40d30167317f6c0ed6e9bed6d6d1341
bargross <barggros@gmail.com>
2019-03-14 19:15:42 +0000

    Changes to MLR object and test file

--- models/MLinearRegression.py
+++ models/MLinearRegression.py
@@ -9,6 +9,7 @@ Created on Mon Mar 11 19:29:35 2019
 import matplotlib.pyplot as plt
 
 # targetted imports
+from pandas import DataFrame
 from json import loads
 from numpy import r_
 from structures.Info import Gatherer
@@ -52,8 +53,8 @@ class MLR():
         self.isFit = False
         
         #
-        self.x_dim = self.info.data.iloc[:,:1].values
-        self.y_dim = self.info.data.iloc[:,:1].values
+        self.x_dim = self.info.data.iloc[info['cutoff']:, :1].values
+        self.y_dim = self.info.data.iloc[info['cutoff']:, :1].values
 
         # get the labels for Multivariate LR and the plot title
         self.info.set_plot_title(info['title'])
@@ -68,26 +69,52 @@ class MLR():
     def add_data_path(self, path=""):
         self.info.set_file_path(path)
 
-    # plots the dataset 
-    def plot(self, x_label="# of x", y_label="# of y"):
-        #
-        # fig,ax = plt.subplots()
+    def reset_data(self):
+        new_data = [
+            self.info.data['Central and Eastern'], 
+            self.info.data['Northern'],	
+            self.info.data['Southern'],
+            self.info.data['Western']
+        ]
+        self.info.data = DataFrame(data=new_data)
 
-        #
-        plt.scatter(self.x_train, self.predictions)
-        plt.plot([self.x_train.min(), self.x_train.max()], [self.x_train.min(), self.x_train.max()], 'k--', lw=4)
-        
-        #
+    def column_to_int(self, column_name=""):
+            if column_name=='Central and Eastern':
+                return 0 
+            elif column_name=='Northern':
+                return 1	
+            elif column_name=='Southern':
+                return 2
+            elif column_name=='Western':
+                return 3
+            else:
+                return None
+
+    # plots the dataset 
+    def plot(self, x_label="", y_label="", plot_type="bar", column=""):
         if x_label != "" and y_label != "":
-            plt.set_xlabel(x_label)
-            plt.set_ylabel(y_label)
-            # ax.set_plot(x=x_label, y=y_label, style='o')
+            plt.xlabel(x_label)
+            plt.ylabel(y_label)
         else:
-            plt.set_xlabel(self.info.xLabel)
-            plt.set_ylabel(self.info.yLabel)
-            #self.info.data.plot(x=self.info.xLabel, y=self.info.yLabel, style='o')
-        #
-        plt.show()
+            plt.xlabel(self.info.xLabel)
+            plt.ylabel(self.info.yLabel)
+        
+        if plot_type == 'bar':
+            plt.bar(self.x_train[self.column_to_int(column)], self.predictions[column], color='blue', align='center')
+            plt.plot([self.x_train.min(), self.x_train.max()], [self.x_train.min(), self.x_train.max()])
+            plt.title(self.info.title, loc='center')
+            plt.show()
+
+        elif plot_type == 'scatter':
+            plt.scatter(self.x_train, self.predictions)
+            plt.plot([self.x_train.min(), self.x_train.max()], [self.x_train.min(), self.x_train.max()])
+            plt.title(self.info.title, loc='center')
+            plt.show()
+        else:
+            plt.subplot(2,1,1)
+            plt.plot([self.x_train.min(), self.x_train.max()], [self.x_train.min(), self.x_train.max()])
+            plt.title(self.info.title, loc='center')
+            plt.show()
     
     # applies cross-validation sampling only (for now)
     def apply_cross_val(self, test_total_size=0, random_state=0):
@@ -106,22 +133,24 @@ class MLR():
     def actual_vs_prediction(self, predict_again=False):
         if predict_again:
             self.get_predictions()
-
-        if self.predictions != None: 
+        if self.predictions != None:
+            print(self.x_train) 
             print(self.predictions)
         else:
             self.predictions = self.regressor.predict(self.x_train)
+            print(self.x_train)
             print(self.predictions)
     
-    def get_regression_coef(self):
+    def print_regression_coef(self):
         if self.isFit:
-            print(self.x_train)
-            for coeff, col_name in  enumerate(self.x_train.columns):
+            train = iter(self.x_train)
+            train = next(train) 
+            for coeff, col_name in enumerate(DataFrame(data=train).columns):
                     print("The coefficient for {} is {}".format(col_name, self.regressor.coef_[0][coeff]))
             # return self.regressor.coef_
         else:
             self.fit()
-            self.get_regression_coef()
+            self.print_regression_coef()
 
     #
     def get_summary(self, summary_type=0):
@@ -134,5 +163,21 @@ class MLR():
             # console
             2: summary.head()
         }[summary_type]
+    
+    def change_data_file(self, path="", xlabel="", ylabel="", title="", cutoff=0):
+        self.info.set_file_path(path)
+        self.info.set_data()
+
+        #
+        self.isFit = False
+        
+        #
+        self.x_dim = self.info.data.iloc[cutoff:, :1].values
+        self.y_dim = self.info.data.iloc[cutoff:, :1].values
+
+        # get the labels for Multivariate LR and the plot title
+        self.info.set_plot_title(title)
+        self.info.set_x_label(xlabel)
+        self.info.set_y_label(ylabel)
         
             
--- test.py
+++ test.py
@@ -8,21 +8,25 @@ from numpy import r_
 from models.MLinearRegression import MLR
 
 info = {
-    'title': 'Multivariate Analysis (GPD/Capita) in Europe', 
+    'title': 'Multivariate Analysis of Income per (GPD/Capita) in Europe', 
     'x': 'Measured', 
     'y': 'Predicted', 
-    'path': 'data/AVG_GDP_Europe.csv'
+    'path': 'data/AVG_GDP_Europe.csv',
+    'cutoff': 77
 }
 
 # Variables
-sample_split = 0.20 # sampling split -> 20% Test | 80% train
+sample_split = 0.2 # sampling split -> 20% Test | 80% train
 split_random_state = 1
 
 # create the MLR object
 multivariate_analysis = MLR(dumps(info))
 
-# set the x and y dimensions
-multivariate_analysis.set_dimensions(multivariate_analysis.info.data.iloc[:, :-1], multivariate_analysis.info.data.iloc[:, :1])
+
+# ==================================================================================
+
+# set the data to the dimensions we want
+# multivariate_analysis.reset_data()
 
 # summary before regression
 summary = multivariate_analysis.get_summary(1)
@@ -33,9 +37,9 @@ multivariate_analysis.apply_cross_val(sample_split, split_random_state)
 
 # fit the data
 multivariate_analysis.fit()
-print(multivariate_analysis.x_train.__sizeof__())
-print(multivariate_analysis.x_train.get_predictions().__sizeof__())
-multivariate_analysis.get_regression_coef()
+
+# print coefficients
+multivariate_analysis.print_regression_coef()
 
 # original summary
 print("Summary before M-Linear Regression (R Style Summary)")
@@ -48,8 +52,83 @@ print("-----------------------------------------------")
 print(multivariate_analysis.get_predictions())
 print("-----------------------------------------------")
 
-# print(multivariate_analysis.predictions)
-# print(multivariate_analysis.x_test.__sizeof__())
+# plot the results
+multivariate_analysis.plot(column='Central and Eastern')
+multivariate_analysis.plot(column='Northern') 
+multivariate_analysis.plot(column='Southern')
+multivariate_analysis.plot(column='Western')  
+
+# ==================================================================================
+
+multivariate_analysis.change_data_file(
+    path='data/AVG_Education_Europe_(mean years of schooling).csv',
+    xlabel=info['x'],
+    ylabel=info['y'],
+    title='Multivariate Analysis of mean years of schooling in Europe'
+)
+
+# summary before regression
+summary = multivariate_analysis.get_summary(1)
+
+
+# apply the sampling technique
+multivariate_analysis.apply_cross_val(sample_split, split_random_state)
+
+# fit the data
+multivariate_analysis.fit()
+
+# print coefficients
+multivariate_analysis.print_regression_coef()
+
+# original summary
+print("Summary before M-Linear Regression (R Style Summary)")
+print("-----------------------------------------------")
+print(multivariate_analysis.get_summary(1))
+print("-----------------------------------------------")
+print()
+print("Predictions: ")
+print("-----------------------------------------------")
+print(multivariate_analysis.get_predictions())
+print("-----------------------------------------------")
 
 # plot the results
-multivariate_analysis.plot() 
+multivariate_analysis.plot(column='Central and Eastern')
+multivariate_analysis.plot(column='Northern') 
+multivariate_analysis.plot(column='Southern')
+multivariate_analysis.plot(column='Western')  
+
+# ==================================================================================
+
+multivariate_analysis.change_data_file(
+    path='data/AVG_Life_expectancy_Europe.csv',
+    xlabel=info['x'],
+    ylabel=info['y'],
+    title='Multivariate Analysis of Life Expectancy in Europe'
+)
+
+# summary before regression
+summary = multivariate_analysis.get_summary(1)
+
+
+# apply the sampling technique
+multivariate_analysis.apply_cross_val(sample_split, split_random_state)
+
+# fit the data
+multivariate_analysis.fit()
+
+# print coefficients
+multivariate_analysis.print_regression_coef()
+
+# original summary
+print("Summary before M-Linear Regression (R Style Summary)")
+print("-----------------------------------------------")
+print(multivariate_analysis.get_summary(1))
+print("-----------------------------------------------")
+print()
+print("Predictions: ")
+print("-----------------------------------------------")
+print(multivariate_analysis.get_predictions())
+print("-----------------------------------------------")
+
+# plot the results
+multivariate_analysis.plot()
