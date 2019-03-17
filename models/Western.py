a86c34fa693a3dc83c38380b878c3257fd46fe02
bargross <barggros@gmail.com>
2019-03-17 19:37:36 +0000

    changes to models

--- models/Western.py
+++ models/Western.py
@@ -8,7 +8,7 @@ from regression.MLinearRegression import MLR
 from matplotlib.colors import ListedColormap
 
 info = {
-    'titles': 'Multivariate Analysis of Income per (GPD/Capita) in Europe', 
+    'title': 'Multivariate Analysis of Income per (GPD/Capita) in Europe', 
     'x': '', 
     'y': '', 
     'file': 'data/Western.csv'
@@ -40,4 +40,28 @@ multivariate_analysis.actual_vs_prediction()
 print("")
 print("")
 # plot the results
-multivariate_analysis.plot(colmap=ListedColormap(['red', 'blue','yellow','green']))
+multivariate_analysis.plot(colmap=ListedColormap(['red','green']))
+
+# ==================================================================================
+
+# change the x dimension to Education
+multivariate_analysis.set_dimensions(x="Education", y="Life Expectancy")
+
+# summary before regression
+summary = multivariate_analysis.get_summary(1)
+
+# apply the sampling technique
+multivariate_analysis.apply_cross_val(sample_split, split_random_state)
+
+# fit the data
+multivariate_analysis.fit()
+
+# compare the original train to the predicted results
+multivariate_analysis.actual_vs_prediction()
+
+# print coefficients
+# multivariate_analysis.print_coef_and_incetercept()
+print("")
+print("")
+# plot the results
+multivariate_analysis.plot(colmap=ListedColormap(['red','green']))
